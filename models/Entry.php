641845b51968a3b18b75e921854750ce5f3a4875
Dabil <dbillett1@gmail.com>
2013-02-15 16:56:15 -0500

    Adds round drop functionality and bug fixes
    Adds:
    
    Round drop recording functionality
    
    Fixes:
    
    Deck Record String to recognize byes and matches in progress
    Deck Creation Priviledges
    Remove Entry now also deletes deck to eliminate hanging deck issue

--- models/Entry.php
+++ models/Entry.php
@@ -5,11 +5,13 @@ class Entry {
   public $player;
   public $deck;
   public $medal;
+  public $drop_round;
 
   static function findByEventAndPlayer($eventname, $playername) {
     $db = Database::getConnection();
     $stmt = $db->prepare("SELECT deck, medal FROM entries WHERE event = ? AND player = ?");
     $stmt->bind_param("ss", $eventname, $playername);
+    $stmt->execute();
     $stmt->store_result();
     $found = false;
     if ($stmt->num_rows > 0) {
@@ -24,18 +26,19 @@ class Entry {
     }
   }
 
+  // TODO: remove ignore functionality
   function __construct($eventname, $playername) {
     $db = Database::getConnection();
-    $stmt = $db->prepare("SELECT deck, medal, ignored FROM entries WHERE event = ? AND player = ?");
+    $stmt = $db->prepare("SELECT deck, medal, ignored, drop_round FROM entries WHERE event = ? AND player = ?");
     $stmt or die($db->error);
     $stmt->bind_param("ss", $eventname, $playername);
     $stmt->execute();
     $this->ignored = 0;
-    $stmt->bind_result($deckid, $this->medal, $this->ignored);
-    if ($stmt->fetch() == NULL) {
-      throw new Exception('Entry for '. $playername .' in '. $eventname .' not found');
-    }
+    $stmt->bind_result($deckid, $this->medal, $this->ignored, $this->drop_round);
 
+    if ($stmt->fetch() == NULL) { 
+            throw new Exception('Entry for '. $playername .' in '. $eventname .' not found');
+    }
     $stmt->close();
 
     $this->event = new Event($eventname);
@@ -58,6 +61,10 @@ class Entry {
         $wins = $wins + 1;
       } else if ($match->playerLost($this->player)) {
         $losses = $losses + 1;
+      } else if ($match->playerBye($this->player)) {
+        $wins = $wins + 1;
+      } else if ($match->playerMatchInProgress($this->player)) {
+          ; // do nothing since match is in progress and there are no results
       } else {
         $draws = $draws + 1;
       }
@@ -74,17 +81,28 @@ class Entry {
     return $this->player->getMatchesEvent($this->event->name);
   }
 
-  function canCreateDeck($username) {
-    if (($this->event->finalized == 0) && (strcasecmp($username, $this->player->name) == 0)) {
-      return true;
-    }
-    $player = new Player($username);
-    if ($player->isSuper()) {
-      return true;
-    }
-    return $this->event->isHost($username) || $this->event->isSteward($username);
+  function canDelete() {
+    $matches = $this->getMatches();
+    return (count($matches) == 0);
   }
 
+  function dropped() {
+    return ($this->drop_round > 0);
+  }
+
+  function canCreateDeck($username) {
+    $player = new Player($username);
+
+    if ($player->isSuper() || 
+        $this->event->isHost($username) || 
+        $this->event->isOrganizer($username) || 
+        strcasecmp($username, $this->player->name) == 0) {
+      return true;
+    }
+    return false;
+  }
+
+  // TODO: Remove ignore functionality
   function setIgnored($new_ignored) {
     $db = Database::getConnection();
     $stmt = $db->prepare("UPDATE entries SET ignored = ? WHERE player = ? and event = ?");
@@ -95,9 +113,27 @@ class Entry {
     $stmt->close();
   }
 
-  function createDeckLink() {
+  function removeEntry() {
+    $db = Database::getConnection();
+    if ($this->deck) {$this->deck->delete();} // if the player being unreg'd entered a deck list, remove it
+    
+    $stmt = $db->prepare("DELETE FROM entries WHERE event = ? AND player = ?");
+    $stmt->bind_param("ss", $this->event->name, $this->player->name);
+    $stmt->execute();
+    $removed = $stmt->affected_rows > 0;
+    $stmt->close();
+
+    $db = Database::getConnection();
+    $stmt = $db->prepare("DELETE FROM standings WHERE event = ? AND player = ?");
+    $stmt->bind_param("ss", $this->event->name, $this->player->name);
+    $stmt->execute();
+    $stmt->close();
+    return $removed;
+  }  
+  
+  function createDeckLink() { // creates a link to enter a deck list once a player is registered for the event
     if ($this->canCreateDeck(Player::loginName())) {
-      return "<a class=\"create_deck_link\" href=\"deck.php?player={$this->player->name}&event={$this->event->name}&mode=create\">[Create Deck]</a>";
+      return "<a class=\"createlink\" class=\"create_deck_link\" href=\"deck.php?player={$this->player->name}&event={$this->event->name}&mode=create\">[Create Deck]</a>";
     } else {
       return "(no deck entered)";
     }
