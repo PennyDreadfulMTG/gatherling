8db03d49e051dd568a9d350f85b28c399447ee64
Dabil <dbillett1@gmail.com>
2013-02-15 18:08:22 -0500

    Text files are now more readable in text editors

--- deckdl.php
+++ deckdl.php
@@ -5,24 +5,26 @@ $id = 0;
 if(isset($_GET['id'])) {$id = $_GET['id'];}
 if(isset($_POST['id'])) {$id = $_POST['id'];}
 
-if ($id == 0) {
+if ($id == 0) { 
   header("location: player.php");
   exit;
-}
+} 
 
 $deck = new Deck($id);
 
 $content = "";
 
-foreach ($deck->maindeck_cards as $card => $qty) {
-  $content .= $qty . " " . $card . "\n";
+foreach ($deck->maindeck_cards as $card => $qty) { 
+  // Æ to AE litigation
+  $card = preg_replace("/\306/", "AE", $card);
+  $content .= $qty . " " . $card . "\r\n"; 
 }
 
-$content .= "\nSideboard\n";
+$content .= "\r\nSideboard\r\n";
 
-foreach ($deck->sideboard_cards as $card => $qty) {
-  $content .= $qty . " " . $card . "\n";
-}
+foreach ($deck->sideboard_cards as $card => $qty) { 
+  $content .= $qty . " " . $card . "\r\n";
+} 
 
 $filename = preg_replace("/ /", "_", $deck->name) . ".txt";
 header("Content-type: text/plain");
