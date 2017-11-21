599046cf982dc15f90f68b57c93def0ebc73b32e
Katelyn Gigante <clockwork.singularity@gmail.com>
2017-11-22 00:32:44 +1100

    Clean up ajax.php for future extensibility.

--- ajax.php
+++ ajax.php
@@ -1,10 +1,27 @@
 <?php session_start();
 require_once 'lib.php';
 
-if (isset($_GET['deck'])) {
+$action = '';
+if (isset($_REQUEST['action']))
+{
+  $action = $_REQUEST['action'];
+}
+// The below is for backwards compat
+else if (isset($_GET['deck'])) {
+  $action = 'deckinfo';
+} elseif (isset($_GET['addplayer']) && isset($_GET['event'])) {
+  $action = 'addplayer';
+} elseif (isset($_GET['delplayer']) && isset($_GET['event'])) {
+  $action = 'delplayer';
+} elseif (isset($_GET['dropplayer']) && isset($_GET['event'])) {
+  $action = 'dropplayer';
+}
+
+$result = array();
+switch ($action){
+  case 'deckinfo':
   $deckid = $_GET['deck'];
   $deck = new Deck($deckid);
-  $result = array();
   $result["id"] = $deckid;
   if ($deck->id != 0) {
     $result["found"] = 1;
@@ -15,9 +32,9 @@ if (isset($_GET['deck'])) {
   } else {
     $result["found"] = 0;
   }
-  json_headers();
-  echo json_encode($result);
-} elseif (isset($_GET['addplayer']) && isset($_GET['event'])) {
+  break;
+
+  case 'addplayer':
   $event = new Event($_GET['event']);
   if ($event->authCheck($_SESSION['username'])) {
     $result = array();
@@ -31,20 +48,30 @@ if (isset($_GET['deck'])) {
     } else {
       $result["success"] = false;
     }
-    json_headers();
-    echo json_encode($result);
   }
-} elseif (isset($_GET['delplayer']) && isset($_GET['event'])) {
+  else
+  {
+    $result['error'] = 'Unauthorized';
+    $result["success"] = false;
+  }
+  break;
+
+  case 'delplayer':
   $event = new Event($_GET['event']);
   if ($event->authCheck($_SESSION['username'])) {
     $old = $_GET['delplayer'];
     $result = array();
     $result['success'] = $event->removeEntry($old);
     $result['player'] = $old;
-    json_headers();
-    echo json_encode($result);
   }
-} elseif (isset($_GET['dropplayer']) && isset($_GET['event'])) {
+  else
+  {
+    $result['error'] = 'Unauthorized';
+    $result["success"] = false;
+  }
+  break;
+
+  case 'dropplayer':
   $event = new Event($_GET['event']);
   if ($event->authCheck($_SESSION['username'])) {
     $result = array();
@@ -57,7 +84,16 @@ if (isset($_GET['deck'])) {
     json_headers();
     echo json_encode($result);
   }
-} else {
-  error_headers();
+  else
+  {
+    $result['error'] = 'Unauthorized';
+    $result["success"] = false;
+  }
+  break;
+
+  default:
+    $result['error'] = "Unknown action '{$action}'";
 }
 
+json_headers();
+echo json_encode($result);
