f98a7a3c72ebf2de7a6ea294c904b12002b8347e
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-01-21 12:47:32 +1100

    We deployed 4.8.0 onto gatherling.com, so let's bump the version number of dev.

--- lib.php
+++ lib.php
@@ -484,7 +484,8 @@ function print_warning_if($conditional) {
 }
 
 function version_tagline() {
-  print "Gatherling version 4.8.0 (\"Zagreus at the end of days / Zagreus lies all other ways / Zagreus comes when time's a maze / And all of history is weeping.\")";
+  print "Gatherling version 4.8.1 (\"Zagreus at the end of days / Zagreus lies all other ways / Zagreus comes when time's a maze / And all of history is weeping.\")";
+  # print "Gatherling version 4.8.0 (\"Zagreus at the end of days / Zagreus lies all other ways / Zagreus comes when time's a maze / And all of history is weeping.\")";
   # print "Gatherling version 4.7.0 (\"People assume that time is a strict progression of cause to effect, but actually — from a non-linear, non-subjective viewpoint — it's more like a big ball of wibbly-wobbly... timey-wimey... stuff.\")";
   # print "Gatherling version 4.5.2 (\"People assume that time is a strict progression of cause to effect, but actually — from a non-linear, non-subjective viewpoint — it's more like a big ball of wibbly-wobbly... timey-wimey... stuff.\")";
   # print "Gatherling version 4.0.0 (\"Call me old fashioned, but, if you really wanted peace, couldn't you just STOP FIGHTING?\")";
