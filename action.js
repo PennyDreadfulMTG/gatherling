42800e57e5bb955f74b203763822e8d5967f01f0
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-03-22 11:30:10 +1100

    Autorefresh header cta

--- action.js
+++ action.js
@@ -1,3 +1,9 @@
-$.get("action.php", function(data) {
-    $("#action").html(data)
-});
+function update_cta() {
+
+    $.get("action.php", function(data) {
+        $("#action").html(data)
+    });
+}
+
+update_cta();
+setInterval(update_cta, 300000);
