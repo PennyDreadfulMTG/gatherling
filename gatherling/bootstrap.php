53e49d047c7e06dfc51bae660c4753dd0f488380
Katelyn Gigante <clockwork.singularity@gmail.com>
2020-08-14 10:56:28 +1000

    Error Reporting

--- gatherling/bootstrap.php
+++ gatherling/bootstrap.php
@@ -21,3 +21,5 @@ if (version_compare(phpversion(), 6) === -1) {
 }
 
 require_once 'config.php';
+
+Sentry\init(['dsn' => 'https://fdfade4631f84653a606228c18e6922a@o233010.ingest.sentry.io/1414048' ]);
