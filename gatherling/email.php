6a8615cc85cfd1fbe30f432e7cc52dfac7c9b410
Thomas David Baker <bakert@gmail.com>
2024-02-14 14:51:58 -0800

    Flesh out password reset

---
+++ gatherling/email.php
@@ -0,0 +1,36 @@
+<?php
+
+// Use Brevo to send an email from us to a single recipient. Brevo supports multiple recipients, attachments, etc. but we don't need that yet.
+function sendEmail($to, $subj, $msg): bool {
+    global $CONFIG;
+
+    $body = [
+        'sender' => ['name' => 'Gatherling', 'email' => 'no-reply@gatherling.com'],
+        'to' => [['name' => $to, 'email' => $to]],
+        'subject' => $subj,
+        'htmlContent' => $msg
+    ];
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, 'https://api.brevo.com/v3/smtp/email');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
+
+    $headers = [];
+    $headers[] = 'Accept: application/json';
+    $headers[] = 'Api-Key: ' . $CONFIG['brevo_api_key'];
+    $headers[] = 'Content-Type: application/json';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    curl_exec($ch);
+    if (curl_errno($ch)) {
+        return false;
+    }
+    $response_code = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+    if ($response_code >= 300) {
+        return false;
+    }
+    return true;
+}
