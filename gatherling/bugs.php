0af47d05e0933668ffae4d66ae54e3482c2ce036
Thomas David Baker <bakert@gmail.com>
2024-08-25 14:23:16 -0700

    Avoid PHP 8.3 warnings by including php tags in included files
    Also take this opportunity to remove the ancient news we've been showing on the
    homepage for many years.
    
    Via vendor/bin/phpcs --standard=PHPCompatibility --runtime-set testVersion 8.3 --ignore=vendor .

---
+++ gatherling/bugs.php
@@ -0,0 +1,4 @@
+<?php
+
+// This file will be included directly in the home page. See news.php for something similar with actual content.
+
--- gatherling/updates.php
+++ gatherling/updates.php
@@ -1,1 +1,3 @@
+<?php
 
+// This file included directly in index.php. See news.php for an example with more content.
