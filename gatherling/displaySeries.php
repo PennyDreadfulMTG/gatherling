3b6e2b2b7e0d80677f104b45cd0a0f5d7a45bb1a
Thomas David Baker <bakert@gmail.com>
2024-08-29 09:31:43 -0700

    Avoid broken images for series with no logo uploaded
    Fixes #27.

--- gatherling/displaySeries.php
+++ gatherling/displaySeries.php
@@ -13,6 +13,12 @@ $stmt->bind_result($content, $type, $size);
 $stmt->fetch();
 $stmt->close();
 
+// Send a transparent 1x1 png if there's no logo in the db rather than showing a broken image.
+if (!$content) {
+    $type = "image/png";
+    $content = base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII=');
+}
+
 header("Content-length: $size");
 header("Content-type: $type");
 echo $content;
