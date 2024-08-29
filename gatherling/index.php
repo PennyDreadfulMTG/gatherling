f87aa8fae119c5fdf24cfc47c4fbc22f00d9afa1
Thomas David Baker <bakert@gmail.com>
2024-08-29 13:34:01 -0700

    Convert homepage to using page helper function
    It does a tiny bit of deduplication. Might be a nice step towards a bigger refactor.

--- gatherling/index.php
+++ gatherling/index.php
@@ -7,59 +7,47 @@ use Gatherling\Matchup;
 use Gatherling\Player;
 
 include 'lib.php';
-$version = Database::single_result('SELECT version FROM db_version LIMIT 1');
-if ($version < 51) {
-    require 'outofservice.php';
-    return;
+
+function main(): void {
+    ob_start();
+    ?>
+        <div id="gatherling_main" class="box grid_12">
+            <div class="uppertitle">Gatherling</div>
+            <p>Welcome to Gatherling! The place for player-run Magic Online tournaments. Report bugs or give feedback on the <a href="https://discord.gg/F9SrMwV">Discord</a>.</a></p>
+        </div>
+        <div id="maincolumn" class="grid_8">
+            <div class="box">
+                <div class="uppertitle">Active Events</div>
+                <?php activeEvents(); ?>
+            </div>
+            <div class="box">
+                <div class="uppertitle">Upcoming Events</div>
+                <?php upcomingEvents(); ?>
+            </div>
+            <div class="box">
+                <div class="uppertitle">Gatherling Statistics</div>
+                <?php stats(); ?>
+            </div>
+        </div>
+        <div class="grid_4">
+            <div class="box pad">
+                <?php
+                $player = Player::getSessionPlayer();
+                if ($player != null) {
+                    playerBox($player);
+                } else {
+                    loginBox();
+                } ?>
+            </div>
+            <div class="box">
+                <div class="uppertitle">Recent Winners</div>
+                <?php recentWinners(); ?>
+            </div>
+        </div>
+        <div class="clear"></div>
+    <?php
+    echo page('Home', ob_get_clean());
 }
-
-print_header('Home'); ?>
-
-<div id="gatherling_main" class="box grid_12">
-    <div class="uppertitle">Gatherling</div>
-    <p>Welcome to Gatherling! The place for player-run Magic Online tournaments. Report bugs or give feedback on the <a href="https://discord.gg/F9SrMwV">Discord</a>.</a></p>
-</div>
-
-<div id="maincolumn" class="grid_8">
-
-    <div class="box">
-        <div class="uppertitle">Active Events</div>
-        <?php activeEvents(); ?>
-    </div>
-
-    <div class="box">
-        <div class="uppertitle">Upcoming Events</div>
-        <?php upcomingEvents(); ?>
-    </div>
-
-    <div class="box">
-        <div class="uppertitle">Gatherling Statistics</div>
-        <?php stats(); ?>
-    </div>
-
-</div>
-
-<div class="grid_4">
-    <div class="box pad">
-        <?php
-        $player = Player::getSessionPlayer();
-        if ($player != null) {
-            playerBox($player);
-        } else {
-            loginBox();
-        } ?>
-    </div>
-    <div class="box">
-        <div class="uppertitle">Recent Winners</div>
-        <?php recentWinners(); ?>
-    </div>
-
-</div>
-
-<div class="clear"></div>
-
-<?php
-print_footer();
 
 function activeEvents()
 {
@@ -209,4 +197,8 @@ function recentWinners()
     }
     echo '</table>';
     $result->close();
+}
+
+if (basename(__FILE__) == basename($_SERVER['PHP_SELF'])) {
+    main();
 }
