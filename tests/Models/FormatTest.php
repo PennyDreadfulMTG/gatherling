9cf7ec40f8cd3ad8fb0d6e81fad3fae2b2efab2a
Thomas David Baker <bakert@gmail.com>
2024-09-06 17:11:56 -0700

    Add a test of Format using the new database testing stuff

---
+++ tests/Models/FormatTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gatherling\Tests\Models;
+
+use Gatherling\Models\Format;
+use Gatherling\Tests\DatabaseCase;
+
+final class FormatTest extends DatabaseCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->model = new Format('Standard');
+    }
+
+    public function testGetCoreCardsets(): void
+    {
+        $coreSets = $this->model->getCoreCardsets();
+        $this->assertEquals(['M10'], $coreSets);
+    }
+
+    public function testGetBlockCardsets(): void
+    {
+        $blockSets = $this->model->getBlockCardsets();
+        $this->assertEquals(['ELD'], $blockSets);
+    }
+
+    public function testGetExtraCardsets(): void
+    {
+        $extraSets = $this->model->getExtraCardsets();
+        $this->assertEmpty($extraSets);
+    }
+}
