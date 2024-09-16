1271fa79be1931a611530fcdd8281fc29827b3a9
Thomas David Baker <bakert@gmail.com>
2024-09-16 12:44:08 -0700

    Fix bug where textInput didn't know what template to render

--- gatherling/views/Components/TextInput.php
+++ gatherling/views/Components/TextInput.php
@@ -9,6 +9,7 @@ class TextInput extends Component
 
     public function __construct(public string $label, public string $name, mixed $value = '', public int $size = 0, public ?string $reminderText = null, ?string $id = null)
     {
+        parent::__construct('partials/textInput');
         $this->id = $id ?? $this->name;
         $this->value = $value ?? '';
     }
