73bd8e869c10d9dc93baa3c0dc2151e1bf3c3436
Thomas David Baker <bakert@gmail.com>
2024-09-17 16:31:13 -0700

    Convert ratings.php to use a Page
    Definitely some imperfections here and the handling of Pagination leaves a lot
    to be desired but I think this is a good step towards having everything in the
    Page/Component system which will let us do centralized routing/error handling
    and gets the echoes/XSS vulnerabilities out of the system.
    
    It's possible we want to move the db access out of the controllers but that is
    left for another day.

--- gatherling/ratings.php
+++ gatherling/ratings.php
@@ -1,83 +1,52 @@
 <?php
 
-use Gatherling\Models\Database;
-use Gatherling\Models\Pagination;
+use Gatherling\Data\DB;
 use Gatherling\Models\Player;
-use Gatherling\Models\Ratings;
+use Gatherling\Models\Pagination;
+use Gatherling\Views\Pages\Ratings;
 
 require_once 'lib.php';
 
-print_header('Ratings');
-?>
-<div class="grid_10 suffix_1 prefix_1">
-<div id="gatherling_main" class="box">
-<div class="uppertitle"> Ratings </div>
-
-<?php content(); ?>
-
-</div> </div>
-
-<?php print_footer(); ?>
-
-<?php
-
-function content()
+function main(): void
 {
-    $format = 'Composite';
-    if (isset($_POST['format'])) {
-        $format = $_POST['format'];
-    }
-    ratingsForm($format);
-    $min = 20;
-    echo '<br /><center>';
-    currentThrough($format);
-    echo "</center><br />\n";
-    echo '<center>';
-    bestEver($format);
-    echo "</center><br />\n";
-    ratingsTable($format, $min);
-    echo '<br />';
+    $format = $_POST['format'] ?? 'Composite';
+    ['date' => $lastTournamentDate, 'name' => $lastTournamentName] = currentThrough($format);
+    ['rating' => $highestRating, 'player' => $highestRatedPlayer, 't' => $highestRatingTimestamp] = bestEver($format);
+    $minMatches = 20;
+    ['ratings_data' => $ratingsData, 'pagination' => $pagination] = ratingsData($format, $minMatches);
+    $page = new Ratings($format, $lastTournamentDate, $lastTournamentName, $highestRating, $highestRatedPlayer, $highestRatingTimestamp, $minMatches, $ratingsData, $pagination);
+    $page->send();
 }
 
-function ratingsForm($format)
+function ratingsData($format, $minMatches)
 {
-    $ratings = new Ratings();
-    echo "<form action=\"ratings.php\" method=\"post\">\n";
-    echo '<table class="c">';
-    echo '<tr><td>Select a rating to display: ';
-    echo $ratings->formatDropMenuR($format);
-    echo '&nbsp;';
-    echo '<input class="inputbutton" type="submit" name="mode" value="Display Ratings" />';
-    echo "</td></tr>\n";
-    echo "</table></form>\n";
-}
-
-function ratingsTable($format, $min = 20)
-{
-    $db = Database::getConnection();
-    $stmt = $db->prepare('SELECT p.name AS player, r.rating, r.wins, r.losses
-                          FROM ratings r, players p,
-                          (SELECT qr.player AS qplayer, MAX(qr.updated) AS qmax
-                  FROM ratings AS qr
-                  WHERE qr.format = ?
-                  GROUP BY qr.player) AS q
-                  WHERE r.format = ?
-                  AND p.name=r.player
-                  AND q.qplayer=r.player
-                  AND q.qmax=r.updated
-                  AND r.wins + r.losses >= ?
-                          ORDER BY r.rating DESC');
-    $stmt->bind_param('ssd', $format, $format, $min);
-    $stmt->execute() or exit($stmt->error);
-    $stmt->bind_result($playername, $rating, $wins, $losses);
+    $subquery = '
+        SELECT
+            qr.player AS qplayer, MAX(qr.updated) AS qmax
+        FROM
+            ratings AS qr
+        WHERE
+            qr.format = :format
+        GROUP BY qr.player';
+    $sql = "
+        SELECT
+            p.name AS player, r.rating, r.wins, r.losses
+        FROM
+            ratings r,
+            players p,
+            ({$subquery}) AS q
+        WHERE
+            r.format = :format AND p.name = r.player AND q.qplayer = r.player AND q.qmax = r.updated AND r.wins + r.losses >= :min_matches
+        ORDER BY
+            r.rating DESC";
+    $ratings_data = DB::select($sql, ['format' => $format, 'min_matches' => $minMatches]);
     $rank = 0;
-
-    $ratings_data = [];
-    while ($stmt->fetch()) {
+    foreach ($ratings_data as &$data) {
         $rank++;
-        $ratings_data[] = ['rank' => $rank, 'playername' => $playername, 'rating' => $rating, 'wins' => $wins, 'losses' => $losses];
+        $data['rank'] = $rank;
+        $data['playerName'] = $data['player'];
+        $data['player'] = new Player($data['player']);
     }
-    $stmt->close();
 
     $records_per_page = 25;
     $pagination = new Pagination();
@@ -89,71 +58,37 @@ function ratingsTable($format, $min = 20)
     $ratings_data = array_slice($ratings_data, (($pagination->get_page() - 1)
                                     * $records_per_page), $records_per_page);
 
-    echo "<table class=\"c\" width=\"500px\">\n";
-    echo '<tr><td colspan=6 align="center">';
-    echo "<i>Only players with $min or more matches are included.";
-    echo '</td></tr>';
-    echo "<tr><td>&nbsp;</td></tr>\n";
-    echo '<tr><td align="center"><b>Rank</td>';
-    echo '<td><b>Player</td><td align="center">';
-    echo '<b>Rating</td>';
-    echo "<td align=\"center\" colspan=\"3\"><b>Record</td></tr>\n";
-
-    foreach ($ratings_data as $vals) {
-        echo "<tr><td align=\"center\">{$vals['rank']}</td><td>";
-        $player = new Player($vals['playername']);
-        echo $player->linkTo();
-        echo "</td>\n";
-        echo "<td align=\"center\">{$vals['rating']}</td>\n";
-        echo "<td align=\"right\" width=35>{$vals['wins']}&nbsp;</td>\n";
-        echo "<td align=\"center\">-</td><td width=35 align=\"left\">&nbsp;{$vals['losses']}</td></tr>";
-    }
-    echo '</table>';
-    $pagination->render();
-    echo '<br />';
-    echo '<br />';
+    return ['ratings_data' => $ratings_data, 'pagination' => $pagination];
 }
 
-function bestEver($format)
+function bestEver($format): array
 {
-    $db = Database::getConnection();
-    $stmt = $db->prepare('SELECT p.name AS player, r.rating,
-                        UNIX_TIMESTAMP(r.updated) AS t
-                FROM ratings AS r, players AS p,
-                (SELECT MAX(qr.rating) AS qmax
-                FROM ratings AS qr WHERE qr.format = ?) AS q
-                        WHERE format = ?  AND p.name=r.player AND q.qmax=r.rating');
-    $stmt->bind_param('ss', $format, $format);
-    $stmt->execute() or exit($stmt->error);
-    $stmt->bind_result($playername, $rating, $timestamp);
-    $stmt->fetch();
-    $stmt->close();
-
-    printf(
-        "The highest $format rating ever achieved is <b>%d</b>, obtained by <b>%s</b> on %s",
-        $rating,
-        $playername,
-        date('l, F j, Y', $timestamp)
-    );
+    $sql = '
+        SELECT
+            p.name AS player, r.rating, UNIX_TIMESTAMP(r.updated) AS t
+        FROM
+            ratings AS r,
+            players AS p,
+            (
+                SELECT
+                    MAX(qr.rating) AS qmax
+                FROM
+                    ratings AS qr
+                WHERE
+                    qr.format = :format
+            ) AS q
+        WHERE
+            format = :format AND p.name = r.player AND q.qmax = r.rating';
+    return DB::select($sql, ['format' => $format])[0];
 }
 
-function currentThrough($format)
+function currentThrough($format): array
 {
-    $db = Database::getConnection();
-    $stmt = $db->prepare('SELECT MAX(updated) AS m FROM ratings WHERE format = ?');
-    $stmt->bind_param('s', $format);
-    $stmt->execute() or exit($stmt->error);
-    $stmt->bind_result($start);
-    $stmt->fetch();
-    $stmt->close();
-    $stmt = $db->prepare('SELECT name FROM events WHERE start = ?');
-    $stmt->bind_param('s', $start);
-    $stmt->execute() or exit($stmt->error);
-    $stmt->bind_result($name);
-    $stmt->fetch();
-    $stmt->close();
-    $date = date('Y-m-d', strtotime($start));
-    echo "<b>Ratings current through {$date} - <span style=\"color: #D45E28\">{$name}</span></b>";
+    $start = DB::value('SELECT MAX(updated) FROM ratings WHERE format = :format', ['format' => $format]);
+    $name = DB::value('SELECT name FROM events WHERE start = :start', ['start' => $start]);
+    return ['date' => new DateTime($start), 'name' => $name];
 }
 
-?>
+if (basename(__FILE__) == basename($_SERVER['PHP_SELF'])) {
+    main();
+}
