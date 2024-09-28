48e1b7df537c7f6e675b2c11e378443f8b08c3c3
Thomas David Baker <bakert@gmail.com>
2024-09-28 08:19:23 -0700

    Remove our Html class in favor of using Crawler directly
    It wasn't adding much and it causes PHPStan warnings about dynamic properties
    because they are deprecated in PHP8.2 and scheduled for removal in PHP9.

--- tests/Views/Components/TimeTest.php
+++ tests/Views/Components/TimeTest.php
@@ -7,6 +7,7 @@ namespace Gatherling\Tests\Views\Components;
 use Gatherling\Views\Components\Time;
 use Gatherling\Tests\Support\Html;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\DomCrawler\Crawler;
 
 class TimeTest extends TestCase
 {
@@ -16,8 +17,8 @@ class TimeTest extends TestCase
 
         $now = (new \DateTimeImmutable('now', $tz))->getTimestamp();
         $timeComponent = new Time($now, $now);
-        $html = new Html($timeComponent->render());
-        $this->assertEquals("just now", $html->text());
+        $html = new Crawler($timeComponent->render());
+        $this->assertEquals("just now", $html->filter('time')->text());
 
         $specificDate = (new \DateTimeImmutable("2024-02-01 12:00:00", $tz))->getTimestamp();
         $oneYearPreviously = $specificDate - 60 * 60 * 24 * 365;
