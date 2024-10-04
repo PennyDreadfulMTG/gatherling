4452358c03ceae2189120858bca4968bcc6f3b1e
Thomas David Baker <bakert@gmail.com>
2024-10-04 12:58:47 -0700

    Fix Submit component
    This is required for add cardset to work on Admin CP, amongst others.

--- gatherling/Views/Components/Submit.php
+++ gatherling/Views/Components/Submit.php
@@ -6,5 +6,6 @@ class Submit extends Component
 {
     public function __construct(public string $label, public string $name = 'action')
     {
+        parent::__construct('partials/submit');
     }
 }
