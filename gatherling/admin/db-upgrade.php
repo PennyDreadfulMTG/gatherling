9ea2f95c7e34260e0500337caab702055d5df604
Thomas David Baker <bakert@gmail.com>
2024-10-06 13:53:26 -0700

    Avoid type error by reading from typesafe location

--- gatherling/admin/db-upgrade.php
+++ gatherling/admin/db-upgrade.php
@@ -6,6 +6,8 @@ namespace Gatherling\Admin;
 
 use Gatherling\Data\Setup;
 
+use function Gatherling\Views\server;
+
 set_time_limit(0);
 
 require_once __DIR__.'/../lib.php';
@@ -16,6 +18,6 @@ function main(): void
     echo 'done';
 }
 
-if (basename(__FILE__) == basename($_SERVER['PHP_SELF'])) {
+if (basename(__FILE__) == basename(server()->string('PHP_SELF'))) {
     main();
 }
