dbaa8631a296dbfb4c9ee4f7ec138eb9798218c9
Thomas David Baker <bakert@gmail.com>
2024-10-08 10:15:45 -0700

    Use superglobal replacements for better typesafety in infobot.php

--- gatherling/admin/infobot.php
+++ gatherling/admin/infobot.php
@@ -4,21 +4,23 @@ declare(strict_types=1);
 
 use Gatherling\Models\Player;
 
+use function Gatherling\Views\config;
 use function Gatherling\Views\get;
 use function Gatherling\Views\request;
 use function Gatherling\Views\server;
 
 require_once __DIR__.'/../lib.php';
 
-global $CONFIG;
-
 if (strncmp(server()->string('HTTP_USER_AGENT', ''), 'infobot', 7) != 0) {
     exit("<error>You're not infobot!</error>");
 }
 
-if ($_GET['passkey'] != $CONFIG['infobot_passkey']) {
+$passKey = get()->optionalString('passkey');
+if (!$passKey || $passKey != config()->optionalString('infobot_passkey')) {
     exit('<error>Wrong passkey</error>');
 }
+
+$siteName = config()->string('site_name');
 
 // generate a user passkey for verification
 $random_num = mt_rand();
@@ -26,17 +28,17 @@ $key = sha1((string) $random_num);
 $challenge = substr($key, 0, 5);
 $player = Player::findByName(get()->optionalString('username') ?? '');
 if (!$player) {
-    echo "<UaReply>You're not registered on {$CONFIG['site_name']}!</UaReply>";
-
+    echo "<UaReply>You're not registered on {$siteName}!</UaReply>";
     return;
 }
 
 if (strcmp(request()->string('mode', ''), 'verify') == 0) {
     $player->setChallenge($challenge);
-    echo "<UaReply>Your verification code for {$CONFIG['site_name']} is $challenge</UaReply>";
+    echo "<UaReply>Your verification code for {$siteName} is $challenge</UaReply>";
 } elseif (strcmp(request()->string('mode', ''), 'reset') == 0) {
     $player->setPassword($challenge);
-    echo "<UaReply>Your temporary password for {$CONFIG['site_name']} is $challenge</UaReply>";
+    echo "<UaReply>Your temporary password for {$siteName} is $challenge</UaReply>";
 } else {
-    echo "<error>Unknown Action {$_REQUEST['mode']}</error>";
+    $mode = request()->string('mode', '');
+    echo "<error>Unknown Action {$mode}</error>";
 }
