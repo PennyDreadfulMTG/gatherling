9b8db4082eb04d38319f2047b75643050d00aaa1
Thomas David Baker <bakert@gmail.com>
2024-10-07 17:21:07 -0700

    Fix type issues in seriesreport.php

--- gatherling/seriesreport.php
+++ gatherling/seriesreport.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 
 use Gatherling\Views\Pages\SeriesReport;
 
+use function Gatherling\Views\get;
+use function Gatherling\Views\server;
+
 require_once 'lib.php';
 
 function main(): void
 {
-    $seriesName = $_GET['series'] ?? null;
-    $season = $_GET['season'] ?? null;
-    $page = new SeriesReport($seriesName, (int) $season);
+    $seriesName = get()->optionalString('series');
+    $season = get()->int('season', 0);
+    $page = new SeriesReport($seriesName, $season);
     $page->send();
 }
 
-if (basename(__FILE__) == basename($_SERVER['PHP_SELF'])) {
+if (basename(__FILE__) == basename(server()->string('PHP_SELF'))) {
     main();
 }
