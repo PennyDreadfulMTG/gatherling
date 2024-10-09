5fdf9e6afbe1c2c96801cae556425a31513d732f
Thomas David Baker <bakert@gmail.com>
2024-10-09 13:24:59 -0700

    Suppress warnings about properties used in an unorthodox way
    We cast this object to an array and use it as args to another query. How wise
    this is is an open question but let's let psalm know we are at least doing it on
    purpose.

--- gatherling/Models/MetaColorsDTO.php
+++ gatherling/Models/MetaColorsDTO.php
@@ -6,6 +6,8 @@ namespace Gatherling\Models;
 
 class MetaColorsDTO extends DTO
 {
+    /** @psalm-suppress PossiblyUnusedProperty */
     public string $colors;
+    /** @psalm-suppress PossiblyUnusedProperty */
     public int $cnt;
 }
