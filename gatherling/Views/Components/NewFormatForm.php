197e05d29383c2cb8f6b1a79b6bc2e86812a3881
Thomas David Baker <bakert@gmail.com>
2024-10-17 12:32:55 -0700

    Remove bad echoes - this is handled in the mustache template now

--- gatherling/Views/Components/NewFormatForm.php
+++ gatherling/Views/Components/NewFormatForm.php
@@ -12,13 +12,5 @@ class NewFormatForm extends Component
 
     public function __construct(public string $seriesName)
     {
-        echo "<h4>New Format</h4>\n";
-        echo '<form action="admincp.php" method="post">';
-        echo '<input type="hidden" name="view" value="no_view" />';
-        echo '<table class="form c">';
-        echo '<tr><td colspan="2">New Format Name: <input type="text" name="newformatname" STYLE="width: 175px"/></td></tr>';
-        echo '<td colspan="2" class="buttons">';
-        echo '<input class="inputbutton" type="submit" value="Create New Format" name ="action" /></td></tr>';
-        echo'</table></form>';
     }
 }
