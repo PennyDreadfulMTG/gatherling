254bb4cd54e454f6fd0f85b2ddc65dd4d61c6c14
Thomas David Baker <bakert@gmail.com>
2024-10-31 12:47:16 -0700

    Migrate game/platform constants to where they are used
    It would be nice to make this a backed enum and use it instead of ints
    everywhere (even disallowing ints) but for now we get it out of lib.php.

--- gatherling/Views/Components/GameName.php
+++ gatherling/Views/Components/GameName.php
@@ -8,21 +8,25 @@ use Gatherling\Models\Player;
 
 class GameName extends Component
 {
+    const MTGO = 1;
+    const MTGA = 2;
+    const PAPER = 3;
+
     public string $name;
     public ?string $iconClass;
 
-    public function __construct(Player $player, string|int|null $game = 'gatherling', bool $html = true)
+    public function __construct(Player $player, int|string|null $game = 'gatherling', bool $html = true)
     {
         $iconClass = null;
         $name = $player->name ?? '';
         if ($html) {
-            if ($game == MTGO && !empty($player->mtgo_username)) {
+            if ($game == self::MTGO && !empty($player->mtgo_username)) {
                 $iconClass = 'ss ss-pmodo';
                 $name = $player->mtgo_username;
-            } elseif ($game == MTGA && !empty($player->mtga_username)) {
+            } elseif ($game == self::MTGA && !empty($player->mtga_username)) {
                 $iconClass = 'ss ss-parl3';
                 $name = $player->mtga_username;
-            } elseif (($game == PAPER || $game == 'discord') && !empty($player->discord_handle)) {
+            } elseif (($game == self::PAPER || $game == 'discord') && !empty($player->discord_handle)) {
                 $iconClass = 'fab fa-discord';
                 $name = $player->discord_handle;
             } elseif ($game == 'gatherling') {
--- gatherling/lib.php
+++ gatherling/lib.php
@@ -22,10 +22,6 @@ if (php_sapi_name() !== 'cli' && session_status() !== PHP_SESSION_ACTIVE) {
 
 date_default_timezone_set('US/Eastern'); // force time functions to use US/Eastern time
 
-const MTGO = 1;
-const MTGA = 2;
-const PAPER = 3;
-
 function json_headers(): void
 {
     header('Content-type: application/json');
