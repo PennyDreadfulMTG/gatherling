f3f4567d3712e3c268ff306f439d050325b9c2d5
Thomas David Baker <bakert@gmail.com>
2024-11-09 08:38:14 -0800

    Almost entirely clean up formatcp's usage of superglobals
    Still passing $_POST to updateFormat to avoid passing 21 params but at least
    we're doing it at the top level now.

--- gatherling/Views/Components/NewFormatForm.php
+++ gatherling/Views/Components/NewFormatForm.php
@@ -8,8 +8,6 @@ use Gatherling\Views\Components\Component;
 
 class NewFormatForm extends Component
 {
-    public string $action;
-
     public function __construct(public string $seriesName)
     {
     }
