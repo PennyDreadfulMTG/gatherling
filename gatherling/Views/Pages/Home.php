aeda4877dda1aca81a5424b29ce16bed963fe5f5
Thomas David Baker <bakert@gmail.com>
2024-11-09 10:08:36 -0800

    Initialize recent winners empty so we can safely add to it during construction

--- gatherling/Views/Pages/Home.php
+++ gatherling/Views/Pages/Home.php
@@ -23,7 +23,7 @@ class Home extends Page
     /** @var ?array{name: string, link: string} */
     public ?array $mostRecentHostedEvent = null;
     /** @var list<array{eventName: string, eventReportLink: string, playerLink: string, deckLink: string, playerName: string, deckName: string, colorImages: ColorImages}> */
-    public array $recentWinners;
+    public array $recentWinners = [];
     public bool $hasRecentWinners;
 
     /**
