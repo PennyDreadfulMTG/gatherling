d3bec0a9dffb2667dda7c7cf6498ab813a7cabd2
Thomas David Baker <bakert@gmail.com>
2024-11-09 07:36:02 -0800

    Clean up cardsets table, add missing set codes, simplify

--- gatherling/Models/CardSetDto.php
+++ gatherling/Models/CardSetDto.php
@@ -7,10 +7,9 @@ namespace Gatherling\Models;
 class CardSetDto extends Dto
 {
     public string $name;
-    public ?string $code;
+    public string $code;
     public string $released;
     public string $type;
-    public ?string $last_updated;
     public int $count;
     public bool $standard_legal;
     public bool $modern_legal;
--- gatherling/Views/Components/SetList.php
+++ gatherling/Views/Components/SetList.php
@@ -10,22 +10,17 @@ use function Gatherling\Helpers\db;
 
 class SetList extends Component
 {
-    /** @var array<array{name: string, editLink: string, code: string|null, released: string, type: string, lastUpdated: string|null, count: int}> */
+    /** @var array<array{name: string, editLink: string, code: string|null, released: string, type: string, count: int}> */
     public array $sets = [];
 
     public function __construct()
     {
         $sql = '
-            SELECT
-                cs.name, cs.code, cs.released, cs.type, cs.last_updated, COUNT(*) AS `count`
-            FROM
-                cardsets AS cs
-            LEFT JOIN
-                cards AS c ON cs.name = c.cardset
-            GROUP BY
-                cs.name
-            ORDER BY
-                cs.name';
+            SELECT cs.name, cs.code, cs.released, cs.type, COUNT(*) AS `count`
+              FROM cardsets AS cs
+         LEFT JOIN cards AS c ON cs.name = c.cardset
+          GROUP BY cs.name
+          ORDER BY cs.name';
         $sets = db()->select($sql, CardSetDto::class);
         foreach ($sets as $set) {
             $this->sets[] = [
@@ -34,7 +29,6 @@ class SetList extends Component
                 'code' => $set->code,
                 'released' => $set->released,
                 'type' => $set->type,
-                'lastUpdated' => $set->last_updated,
                 'count' => $set->count,
             ];
         }
