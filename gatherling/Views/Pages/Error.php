ec3a4a4f05fde6b24db4103ffb596f00ce5d568e
Thomas David Baker <bakert@gmail.com>
2024-11-12 12:47:58 -0800

    Print stack trace on error page in dev env (only)

--- gatherling/Views/Pages/Error.php
+++ gatherling/Views/Pages/Error.php
@@ -8,7 +8,7 @@ class Error extends Page
 {
     public bool $canRetry;
 
-    public function __construct(public string $requestId, public string $message, int $httpStatusCode)
+    public function __construct(public string $requestId, public string $message, int $httpStatusCode, public string $debug)
     {
         parent::__construct('Error', true);
         http_response_code($httpStatusCode);
