317b61bb53a01ff876d09814bb90308defe2b84b
Thomas David Baker <bakert@gmail.com>
2024-11-19 14:25:51 -0800

    Remove a test that has been typesafed away

--- tests/Models/DeckTest.php
+++ tests/Models/DeckTest.php
@@ -81,8 +81,6 @@ class DeckTest extends DatabaseCase
         $deck->event_id = $this->event->id;
         $deck->save();
 
-        $this->assertNotNull($deck->id);
-
         $deck = new Deck($deck->id);
         $this->assertFalse($deck->new);
         $this->assertEquals($deck->playername, $this->player->name);
