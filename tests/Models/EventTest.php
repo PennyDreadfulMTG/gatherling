98d3e5635d8c3b2911fdd9d98c4279c75cfc9165
Thomas David Baker <bakert@gmail.com>
2025-03-24 21:49:54 -0500

    Move createTestEvent to DatabaseCase so more tests can use it

--- tests/Models/EventTest.php
+++ tests/Models/EventTest.php
@@ -17,67 +17,6 @@ use function Gatherling\Helpers\parseCardsWithQuantity;
 
 class EventTest extends DatabaseCase
 {
-    /**
-     * @param array{
-     *   series?: string,
-     *   host?: string,
-     *   cohost?: string,
-     *   name?: string,
-     *   start?: DateTimeImmutable,
-     *   kvalue?: int,
-     *   format?: string,
-     *   season?: int,
-     *   number?: int,
-     *   mainstruct?: string,
-     *   mainrounds?: int,
-     *   finalstruct?: string,
-     *   finalrounds?: int,
-     *   threadurl?: string,
-     *   reporturl?: string,
-     *   metaurl?: string
-     * } $overrides
-     */
-    private function createTestEvent(array $overrides = []): Event
-    {
-        if (Series::exists('Test Series for Test Event')) {
-            $series = new Series('Test Series for Test Event');
-        } else {
-            $series = new Series('');
-            $series->name = 'Test Series for Test Event';
-            $series->start_day = 'Monday';
-            $series->start_time = '00:00:00';
-            $series->active = 1;
-            $series->save();
-        }
-
-        $host = Player::findOrCreateByName($overrides['host'] ?? 'Test Host');
-        $cohost = null;
-        if (isset($overrides['cohost'])) {
-            $cohost = Player::findOrCreateByName($overrides['cohost']);
-        }
-
-        $event = new Event('');
-        $event->name = $overrides['name'] ?? 'Test Event';
-        $event->host = $host->name;
-        $event->cohost = $cohost->name ?? null;
-        $event->start = $overrides['start'] ?? new DateTimeImmutable('2025-01-01');
-        $event->kvalue = $overrides['kvalue'] ?? 8;
-        $event->format = $overrides['format'] ?? 'Standard';
-        $event->series = $series->name;
-        $event->season = $overrides['season'] ?? 1;
-        $event->number = $overrides['number'] ?? 1;
-        $event->mainstruct = $overrides['mainstruct'] ?? 'Swiss';
-        $event->mainrounds = $overrides['mainrounds'] ?? 1;
-        $event->finalstruct = $overrides['finalstruct'] ?? 'Single Elimination';
-        $event->finalrounds = $overrides['finalrounds'] ?? 1;
-        $event->threadurl = $overrides['threadurl'] ?? '';
-        $event->reporturl = $overrides['reporturl'] ?? '';
-        $event->metaurl = $overrides['metaurl'] ?? '';
-        $event->save();
-        $event = new Event($event->name);
-        return $event;
-    }
-
     public function testStructureSummary(): void
     {
         $event = new Event('');
--- tests/Support/TestCases/DatabaseCase.php
+++ tests/Support/TestCases/DatabaseCase.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Gatherling\Tests\Support\TestCases;
 
+use Gatherling\Models\Event;
+use Gatherling\Models\Player;
+use Gatherling\Models\Series;
 use PHPUnit\Framework\TestCase;
+use Safe\DateTimeImmutable;
 
 use function Gatherling\Helpers\db;
 
@@ -33,5 +37,66 @@ abstract class DatabaseCase extends TestCase
     {
         $classHash = substr(md5(get_class($this)), 0, 7);
         return $this->name() . '_' . $classHash;
+    }
+
+    /**
+     * @param array{
+     *   series?: string,
+     *   host?: string,
+     *   cohost?: string,
+     *   name?: string,
+     *   start?: DateTimeImmutable,
+     *   kvalue?: int,
+     *   format?: string,
+     *   season?: int,
+     *   number?: int,
+     *   mainstruct?: string,
+     *   mainrounds?: int,
+     *   finalstruct?: string,
+     *   finalrounds?: int,
+     *   threadurl?: string,
+     *   reporturl?: string,
+     *   metaurl?: string
+     * } $overrides
+     */
+    protected function createTestEvent(array $overrides = []): Event
+    {
+        if (Series::exists('Test Series for Test Event')) {
+            $series = new Series('Test Series for Test Event');
+        } else {
+            $series = new Series('');
+            $series->name = 'Test Series for Test Event';
+            $series->start_day = 'Monday';
+            $series->start_time = '00:00:00';
+            $series->active = 1;
+            $series->save();
+        }
+
+        $host = Player::findOrCreateByName($overrides['host'] ?? 'Test Host');
+        $cohost = null;
+        if (isset($overrides['cohost'])) {
+            $cohost = Player::findOrCreateByName($overrides['cohost']);
+        }
+
+        $event = new Event('');
+        $event->name = $overrides['name'] ?? 'Test Event';
+        $event->host = $host->name;
+        $event->cohost = $cohost->name ?? null;
+        $event->start = $overrides['start'] ?? new DateTimeImmutable('2025-01-01');
+        $event->kvalue = $overrides['kvalue'] ?? 8;
+        $event->format = $overrides['format'] ?? 'Standard';
+        $event->series = $series->name;
+        $event->season = $overrides['season'] ?? 1;
+        $event->number = $overrides['number'] ?? 1;
+        $event->mainstruct = $overrides['mainstruct'] ?? 'Swiss';
+        $event->mainrounds = $overrides['mainrounds'] ?? 1;
+        $event->finalstruct = $overrides['finalstruct'] ?? 'Single Elimination';
+        $event->finalrounds = $overrides['finalrounds'] ?? 1;
+        $event->threadurl = $overrides['threadurl'] ?? '';
+        $event->reporturl = $overrides['reporturl'] ?? '';
+        $event->metaurl = $overrides['metaurl'] ?? '';
+        $event->save();
+        $event = new Event($event->name);
+        return $event;
     }
 }
